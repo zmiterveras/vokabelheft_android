4bf4d60480483855d79d364fd3d2c7df21477788
ZMV <zmvph79@gmail.com>
2025-04-28 15:15:23 +0300

    Added strings resources, corrected Welcome

--- app/src/main/java/com/zmv/vokabelheft/screens/Welcome.kt
+++ app/src/main/java/com/zmv/vokabelheft/screens/Welcome.kt
@@ -6,25 +6,41 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.zmv.vokabelheft.ui.theme.VokabelheftTheme
+import com.zmv.vokabelheft.R
 
 @Composable
-fun Welcome(navController: NavHostController) {
+fun Welcome(navController: NavHostController, modifier: Modifier = Modifier) {
     Box(
-        modifier = Modifier.fillMaxSize(),
+        modifier = modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-       Column(horizontalAlignment = Alignment.CenterHorizontally) {
+       Column(
+           modifier = modifier,
+           horizontalAlignment = Alignment.CenterHorizontally) {
+           Text(text = stringResource(R.string.welcome),
+               fontSize = 20.sp,
+               fontWeight = FontWeight.SemiBold)
+
+           Spacer(modifier = modifier.height(20.dp))
+
+           Text(text = stringResource(R.string.choose_language))
+
+           Spacer(modifier = modifier.height(20.dp))
+
            Button(onClick = { }) {
-               Text(text = "English")
+               Text(text = stringResource(R.string.en_button))
            }
-           Spacer(modifier = Modifier.height(20.dp))
+           Spacer(modifier = modifier.height(20.dp))
            Button(onClick = { }) {
-               Text(text = "Deutsch")
+               Text(text = stringResource(R.string.de_button))
            }
        }
     }
