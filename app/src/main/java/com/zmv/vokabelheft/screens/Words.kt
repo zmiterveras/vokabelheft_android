a6e0306856941aaba08179653f876e6086188139
ZMV <zmvph79@gmail.com>
2025-06-07 17:39:16 +0300

    added navigation from Words to Word

--- app/src/main/java/com/zmv/vokabelheft/screens/Words.kt
+++ app/src/main/java/com/zmv/vokabelheft/screens/Words.kt
@@ -1,5 +1,6 @@
 package com.zmv.vokabelheft.screens
 
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,6 +19,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -43,7 +45,7 @@ fun Words(navController: NavHostController, modifier: Modifier = Modifier) {
         val randomItems: List<List<String>> = getRandomList("String")
         LazyColumn(modifier.fillMaxWidth().fillMaxHeight(0.9f)) {
             items(randomItems) { randomItem ->
-                WordCell(randomItem[0], randomItem[1])
+                WordCell(randomItem[0], randomItem[1], navController)
                 Spacer(modifier.height(10.dp))
             }
         }
@@ -52,8 +54,16 @@ fun Words(navController: NavHostController, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun WordCell(textItem: String, translate: String, modifier: Modifier = Modifier) {
-    Column(modifier.padding(15.dp)) {
+fun WordCell(textItem: String, translate: String, navController: NavHostController, modifier: Modifier = Modifier) {
+    Column(modifier
+        .padding(15.dp)
+        .pointerInput(Unit) {
+            detectTapGestures(
+                onPress = {
+                    navController.navigate(NavRoutes.Word.route)
+                }
+            )
+        }) {
         Text(style = MaterialTheme.typography.bodyLarge,
             text = textItem)
         Spacer(modifier.height(7.dp))
