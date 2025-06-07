0a52bd8afe72cf18b2dfddd0b00f90fd25d55f50
ZMV <zmvph79@gmail.com>
2025-06-07 11:18:06 +0300

    corrected NavRoutes.kt

--- app/src/main/java/com/zmv/vokabelheft/NavRoutes.kt
+++ app/src/main/java/com/zmv/vokabelheft/NavRoutes.kt
@@ -5,5 +5,9 @@ sealed class NavRoutes(val route: String) {
     object Words : NavRoutes("words")
     object Word : NavRoutes("word")
     object AddEdit: NavRoutes("add_edit")
+    object TrainingSelection: NavRoutes("training_selection")
+    object Question: NavRoutes("question")
+    object WordResult: NavRoutes("word_result")
+    object Results: NavRoutes("results")
     object Test: NavRoutes("test")
 }
