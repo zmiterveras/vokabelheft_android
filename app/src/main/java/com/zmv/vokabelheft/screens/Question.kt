db89ab9026f37936382cdd69da50fd5ec5f3ae17
ZMV <zmvph79@gmail.com>
2025-06-07 16:43:41 +0300

    corrected Question.kt

--- app/src/main/java/com/zmv/vokabelheft/screens/Question.kt
+++ app/src/main/java/com/zmv/vokabelheft/screens/Question.kt
@@ -3,8 +3,12 @@ package com.zmv.vokabelheft.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -12,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.zmv.vokabelheft.R
@@ -22,13 +27,19 @@ fun Question(navController: NavHostController,
     Column(modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = stringResource(R.string.translate_word))
-        Text(text = translate)
+        Text(text = stringResource(R.string.translate_word),
+            style = MaterialTheme.typography.bodyLarge)
+        Spacer(Modifier.height(7.dp))
+        Text(text = translate,
+            style = MaterialTheme.typography.titleLarge)
+        Spacer(Modifier.height(15.dp))
         TextField(
             value = "",
             onValueChange = {}
         )
-        Row {
+        Spacer(Modifier.height(15.dp))
+        Row(modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly) {
             Button(onClick = {}) {
                 Text(text = stringResource(R.string.ok))
             }
